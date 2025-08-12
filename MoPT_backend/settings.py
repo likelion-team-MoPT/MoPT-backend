4b0065b93729f840cff6870a91a393e827d4e8d8
kimdonghan119 <ehdgksemforh@naver.com>
2025-08-12 14:38:18 +0900

    Update settings.py
    Fix:settings.py

--- MoPT_backend/settings.py
+++ MoPT_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z*xv3#1bm*_8sp+01)sbg^vv%y3ibwlpc-u=(7@on25*o3_28t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
