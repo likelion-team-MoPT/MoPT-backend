8b11c39981428657af5f3209fc1b64da33442c2f
songh020202@gmail.com <songhe02@naver.com>
2025-08-18 10:51:29 +0900

    users/integrations add

--- MoPT_backend/urls.py
+++ MoPT_backend/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
@@ -20,6 +21,7 @@ from ninja import NinjaAPI
 from ai_insights.api import router as insights_router
 from campaigns.api import router as campaigns_router
 from home.api import router as home_router
+from integrations.api import router as integrations_router
 from reports.api import router as reports_router
 from users.api import router as users_router  # 최소 router 하나
 
@@ -30,6 +32,7 @@ api.add_router("/campaigns", campaigns_router)
 api.add_router("/home", home_router)
 api.add_router("/reports", reports_router)
 api.add_router("/users", users_router)
+api.add_router("/integrations", integrations_router)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
