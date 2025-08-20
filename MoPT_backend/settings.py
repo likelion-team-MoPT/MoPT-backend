5f050264d079f7ea9331de18348e105848fb66f7
kimdonghan <kimdonghan119>
2025-08-20 18:01:22 +0900

    Add django-extensions to poetry dependencies

--- MoPT_backend/settings.py
+++ MoPT_backend/settings.py
@@ -61,8 +61,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 ROOT_URLCONF = "MoPT_backend.urls"
 
 TEMPLATES = [
@@ -122,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 LANGUAGE_CODE = "ko-kr"
 TIME_ZONE = "Asia/Seoul"
-USE_I18N = True
+# USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
