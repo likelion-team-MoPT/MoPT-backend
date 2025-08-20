0f2fc78cb03f4ae627f0c4bc9e7baf291aacd40d
kimdonghan <kimdonghan119>
2025-08-20 17:36:13 +0900

    Fix: urls.py merge conflict 해결

--- MoPT_backend/urls.py
+++ MoPT_backend/urls.py
@@ -1,3 +1,19 @@
+"""MoPT_backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
 from django.contrib import admin
 from django.urls import include, path
 from ninja import NinjaAPI
@@ -11,9 +27,9 @@ from users.api import router as users_router
 
 api = NinjaAPI(title="MoPT API", version="1.0", urls_namespace="api-v1")
 
-api.add_router("/home", home_router, tags=["홈"])
 api.add_router("/insights", insights_router, tags=["AI인사이트"])
 api.add_router("/campaigns", campaigns_router, tags=["캠페인"])
+api.add_router("/home", home_router, tags=["홈"])
 api.add_router("/reports", reports_router, tags=["리포트"])
 api.add_router("/users", users_router, tags=["내 정보"])
 
