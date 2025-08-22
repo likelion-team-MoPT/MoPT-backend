d731b83d4fa4485367e4547a107e590fef3b686f
songh020202@gmail.com <songhe02@naver.com>
2025-08-23 05:50:09 +0900

    users: seed

--- MoPT_backend/settings.py
+++ MoPT_backend/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
 ]
 ROOT_URLCONF = "MoPT_backend.urls"
 
